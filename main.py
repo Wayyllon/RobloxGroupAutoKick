ff39910b624964fb5535265eb9ecb5eb27b23f10
Wayyllon <61859666+Wayyllon@users.noreply.github.com>
2023-08-03 22:27:58 -0400

    Update main.py
    i dont feel like writing another fucking description the people who use this code dont even know what a python is

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ TimesExiled = 0
 TimesIterated = 0
 
 #Variables
+CheckFrequency = 120 #How frequently the program will check the group & kick banned members.
+RemovePosts = True #Set to "False" if you would like for posts created by users to be preserved after being banned, otherwise leave it set to "True".
 BlackListedGroupId = 1234567890 #The ID of the group youd like the exclude the user ID's From.
 BlackListedUserIds = {123456789,1234567890} #The User ID's you'd like to exclude from the group.
-CheckFrequency = 120 #How frequently the program will check the group & kick banned members.
+BroadcastRuntime = True #Set to "False" if you dont want it to display anything in the output, otherwise leave it set to "True". (This is somewhat a debug setting, leave it on for debugging.)
 Cookies = "" #The cookie you can find under Ctrl+I -> Storage -> Cookies -> .ROBLOSECURITY
+
 #Everything Else (only modify this stuff if you know what you're doing)
+
+#Set Cookies
 Session = requests.Session()
 Session.cookies[".ROBLOSECURITY"] = Cookies
 
+#Functions
 def ReValidateSession():
     SessionRequest1 = Session.post(
         url="https://auth.roblox.com/"
@@ -30,11 +36,13 @@ def DeletePosts(GroupId,UserId):
     print(DeleteResponse)
     if DeleteResponse != 200:
         if DeleteResponse == 403:
-            print("ReValidating Session & Retrying.")
+            if BroadcastRuntime == True:
+                print("ReValidating Session & Retrying.")
             ReValidateSession()
             DeletePosts(GroupId,UserId)
     else:
-        print("Successfully filtered wall posts.")
+        if BroadcastRuntime == True:
+            print("Successfully filtered wall posts.")
 
 def FilterWall():
     for UUID in BlackListedUserIds:
@@ -51,12 +59,14 @@ def KickUser(GroupId,UserId):
 ReValidateSession()
 
 while True:
-
     TimesIterated = TimesIterated+1
-    print("Checking for Iteration | "+str(TimesIterated))
+    if BroadcastRuntime == True:
+        print("Checking for Iteration | "+str(TimesIterated))
+    
     if TimesIterated % 10 == 0 and TimesIterated != 0:
         ReValidateSession()
-    FilterWall()
+    if RemovePosts == True:
+        FilterWall()
     for UUID in BlackListedUserIds:
         UserGroupsDataRAW = requests.get("https://groups.roblox.com/v2/users/"+str(UUID)+"/groups/roles")
         UserGroupsData = UserGroupsDataRAW.json()
@@ -65,7 +75,8 @@ while True:
             Id = Group["id"]
             if BlackListedGroupId == Id:
                 TimesExiled= TimesExiled+1
-                print("Match | "+str(TimesExiled)+" | Found; Exiling.")
+                if BroadcastRuntime == True:
+                    print("Match | "+str(TimesExiled)+" | Found; Exiling.")
                 KickUser(BlackListedGroupId,UUID)
                 
     time.sleep(CheckFrequency)
